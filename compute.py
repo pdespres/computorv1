0a6bae1f241b5beb6a08d06aebd90dda79e963ad
pdespres <33380838+pdespres@users.noreply.github.com>
2017-12-23 23:33:27 +0100

    day 1

---
+++ compute.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# espece de encoding: utf-8 putain cong
+import sys
+
+def usage():
+	print "usage: %s [-?] [polynomial function]" % sys.argv[0]
+	sys.exit(42)
+
+if __name__ == "__main__":
+	argc = len(sys.argv)
+	if argc not in range(2, 4):
+		usage()
+	if argc == 3
+		#traitement params
+		if (1 != 1):
+			#?
+		else
+			usage()
+	computorv1(sys.argv[-1])
