0f72bbaf76def8f8882c5830293b90a514303e5e
pdespres <33380838+pdespres@users.noreply.github.com>
2017-12-23 23:48:51 +0100

    day 1

--- compute.py
+++ compute.py
@@ -2,18 +2,21 @@
 # espece de encoding: utf-8 putain cong
 import sys
 
+def computorv1(polynom):
+	sys.exit(42)
+
 def usage():
-	print "usage: %s [-?] [polynomial function]" % sys.argv[0]
+	print "usage: python %s [-?] [polynomial function]" % sys.argv[0]
 	sys.exit(42)
 
 if __name__ == "__main__":
 	argc = len(sys.argv)
 	if argc not in range(2, 4):
 		usage()
-	if argc == 3
+	if argc == 3:
 		#traitement params
-		if (1 != 1):
-			#?
-		else
+		if 1 != 1:
+			print "params a faire"
+		else:
 			usage()
 	computorv1(sys.argv[-1])
