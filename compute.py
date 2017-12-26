1468b4f8b865f6c59f816ee2d00c5cf6e6232f21
pdespres <33380838+pdespres@users.noreply.github.com>
2017-12-26 19:20:47 +0100

    day 1

--- compute.py
+++ compute.py
@@ -1,27 +1,46 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 2.7
 # waloo le encoding: utf-8 de malade
 import sys
 import re
 
 def parser(string):
-	string = string.replace(' ', '').replace('*', '')
-	regex = r"(^[0-9xX^+=-]+$)"
-	if not re.match(regex, string):
-		exit_error("Unknown character in polynomial function")
-	
-	return("")
+	string = string.lower().replace(' ', '')
+	#rajout coloration syntaxique?
+	regex = r"([^0-9x.*^+=-]+)"
+	p = re.search(regex, string)
+	if p != None:
+		exit_error("unkown string(s) " + ', '.join("'" + x + "'" for x in re.findall(regex, string, re.I)) + \
+			" in the argument")
+	if string.count("=") != 1:
+		exit_error("the argument is not an equation")
+	regex = r"[*]{2,}"
+	p = re.search(regex, string)
+	if p != None:
+		exit_error("too many '" + p.group(0) + "' in the argument")
+	string = string.replace('*', '')
+	split = string.split('=')
+	left = split[0].replace('-', '+-').split('+')
+	right = split[1].replace('-', '+-').split('+')
+	if left[0] == '':
+		exit_error("equation left side is blank")
+	if right[0] == '':
+		exit_error("equation right side is blank")
+	print left
+	print right
+	return(left, right)
 
 def computorv1(string):
-	parser(string)
+	split_eq = parser(string)
 	return
 
 def usage():
-	print "usage: python %s [-?] [polynomial function]" % sys.argv[0]
+	print "usage: python %s [-?] [polynomial equation]" % sys.argv[0]
 	sys.exit(0)
 
 def exit_error(error_mes):
 	if error_mes != "":
-		sys.exit(error_mes)
+		#sys.exit("Error: " + error_mes)
+		print("Error: " + error_mes)
 	else:
 		sys.exit(42)
 
@@ -31,8 +50,21 @@ if __name__ == "__main__":
 		usage()
 	if argc == 3:
 		#traitement params
-		if 1 != 1:
-			print "params a faire"
-		else:
+		#if ?:
+		#	?
+		#else:
 			usage()
-	computorv1(sys.argv[-1])
+	if sys.argv[-1].lower() == "-test":
+		test = [
+		"1xsd¨^4=gddd sz",
+		"5 * X^0 + 4 * X^1 = - 9.3 * X^2 = 1 * X^0",
+		"5 *** x^1 = 0",
+		"4 * X =",
+		"5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"
+		]
+		for s in test:
+			print ""
+			print "Equation: ", s
+			computorv1(s)
+	else:
+		computorv1(sys.argv[-1])
