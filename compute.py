8dd8553221d2491fd8e612242370777421944c88
pdespres <pdespres@student.42.fr>
2018-01-03 13:45:53 +0100

    day 3

--- compute.py
+++ compute.py
@@ -10,46 +10,42 @@ def parser(string):
 	regex = r"([^0-9x.*^ +=-]+)"
 	p = re.search(regex, string, re.I)
 	if p != None:
-		offset = 0
-		str_color = ""
-		for m in re.finditer(regex, string, re.I):
-			str_color += string[offset:m.start()] + "\033[7;31m" + m.group(0) + "\033[0m"
-			offset = m.end()
-		str_color += string[offset:]
-		exit_error(str_color + "  unkown string(s) " + ', '.join("'" + x + "'" for x in re.findall(regex, string, re.I)) + \
-			" in the equation")
+		exit_error("unkown string(s) " + ', '.join("'" + x + "'" for x in re.findall(regex, string, re.I)) + \
+			" in the argument", string, regex)
 
 	#looking for the right number of '=' (start of syntax errors)
 	if string.count("=") != 1:
-		exit_error("the argument is not an equation")
+		exit_error("the argument is not an equation", string, r"=")
 
 	#split each side, then split on +- (except first one)
-	string = string.lower().replace(' ', '')
+	#string = string.lower().replace(' ', '')
 	split = string.split('=')
 	left = re.sub(r"(?<=.)-", "+-", split[0]).split('+')
 	right = re.sub(r"(?<=.)-", "+-", split[1]).split('+')
 
 	#looking for blanks
-	if (len(left) == 1 and left[0] == ''):
-		exit_error("equation left side is blank")
-	if (len( right) == 1 and right[0] == ''):
-		exit_error("equation right side is blank")
+	if (len(left) == 1 and ' '.join(left[0].split()) == ''):
+		exit_error("equation left side is blank", "_" + original, r"_")
+	if (len( right) == 1 and ' '.join(right[0].split()) == ''):
+		exit_error("equation right side is blank", original + "_", r"_")
 
 	#check format for each side
-	regex = r"^-?[0-9]*\.?[0-9]*\*?(x?$|x\^[0-9]+)"
+	regex = r"^\s*-?\s*[0-9]*\s*\.?\s*[0-9]*\s*\*?\s*(x?$|x\s*\^\s*[0-9]+)\s*"
 	for s in left:
-		if (not s or s == '-'):
+		if (not s.strip() or s.strip() == '-'):
 			exit_error("equation left side has a blank after a +/-")
-		p = re.match(regex, s)
+		p = re.match(regex, s, re.I)
 		if p == None:
-			exit_error("syntax error " + s)
+			s = s.strip()
+			exit_error("syntax error " + s, original, "", s)
 		s = s.replace('*', '').replace('^', '')
 	for s in right:
 		if (not s or s == '-'):
 			exit_error("equation right side has a blank after a +/-")
-		p = re.match(regex, s)
+		p = re.match(regex, s, re.I)
 		if p == None:
-			exit_error("syntax error " + s)
+			s = s.strip()
+			exit_error("syntax error " + s, original, "", s)
 		s = s.replace('*', '').replace('^', '')
 
 	#if at last one error and not in test, prog quits
@@ -68,13 +64,19 @@ def usage():
 	print "usage: python %s [-?] [polynomial equation]" % sys.argv[0]
 	sys.exit(0)
 
-def exit_error(error_mes, end_prog=False):
+def exit_error(error_mes, string, regex, find = ""):
 	exit_error.status = True
+	offset = 0
+	str_color = ""
+	if (regex != ""):
+		for m in re.finditer(regex, string, re.I):
+			str_color += string[offset:m.start()] + "\033[7;31m" + m.group(0) + "\033[0m"
+			offset = m.end()
+		str_color += string[offset:]
+	elif (find != ""):
+		str_color = string.replace(find, "\033[7;31m" + find + "\033[0m")
 	if error_mes != "":
-		if end_prog:
-			sys.exit("Error: " + error_mes)
-		else:
-			print("Error: " + error_mes)
+		print("Error: " + str_color + "  " + error_mes)
 	else:
 		sys.exit(42)
 
--- test.py
+++ test.py
@@ -3,17 +3,34 @@
 
 import compute
 
+lib = [
+"caracteres en plus",
+"plusieurs =",
+"mauvaise syntaxe",
+"equation incomplete",
+"equation incomplete 2",
+"mauvaise syntaxe 2",
+"manque exposant",
+"ok 1",
+"naturel"
+]
+
 test = [
 "1xsd¨^4=gddd sz",
 "5 * X^0 + 4 * X^1 = - 9.3 * X^2 = 1 * X^0",
 "5 *** x^1 = 0",
-"4 * X =",
-"5 * X^0 + 4 * X^1 = 5 * X^0 + 4 * X^1 -",
-"5 * X^0 + 4.3.2 * X^1 = 5 * X^0 + 4..16 * X^1 -",
-"5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"
+"4 * X = ",
+"5 * X^0 + 4 * X^1 = 5 * X^0 + 4 * X^1 - ",
+"5 * X^0 + 4.3.2 * X^1 = 5 * X^0 + 4..16 * X^1",
+"5 * X^0 + 4 * X^1 - 9.3 * X^ = 1 * X^0",
+"5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0",
+" -5 + 4X + X^2 -9.3X^2 = -1 +X"
 ]
 
+i = 0
 for s in test:
 	print ""
+	print lib[i]
 	print "Equation: ", s
+	i += 1
 	compute.computorv1(s)
